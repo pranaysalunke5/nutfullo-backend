8074ce288218dda8abc9cb220385b679517230b2
Pranay Salunke <pranaysalunke5@gmail.com>
2026-04-14 12:59:47 +0530

    cmd

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -43,7 +43,7 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT) || 587,
+  port: parseInt(process.env.EMAIL_PORT) || 2525,
   secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
