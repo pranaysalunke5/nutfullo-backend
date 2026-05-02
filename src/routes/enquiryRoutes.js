edd0ee8f6fd86911bf2516ff6f71722fec9b8f6e
Pranay Salunke <pranaysalunke5@gmail.com>
2026-05-02 15:25:25 +0530

    cmd

--- src/routes/enquiryRoutes.js
+++ src/routes/enquiryRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { createEnquiry, getAllEnquiries, updateEnquiryStatus } from '../controllers/enquiryController.js';
+import { createEnquiry, deleteEnquiry, getAllEnquiries, updateEnquiryStatus } from '../controllers/enquiryController.js';
 
 router.post('/create', createEnquiry);
 
 // routes/enquiryRoutes.js
 router.get('/all', getAllEnquiries); 
 router.patch('/status/:id', updateEnquiryStatus);
+router.delete('/:id', deleteEnquiry);
 
 export default router;
